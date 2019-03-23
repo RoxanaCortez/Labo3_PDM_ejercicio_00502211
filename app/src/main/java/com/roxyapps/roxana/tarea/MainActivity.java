5518401d223edd40ed6cf1beed65fe7571f9437c
RoxanaCortez <00502211@uca.edu.sv>
2019-03-22 23:25:36 -0600

    Intent explicito completo

--- app/src/main/java/com/roxyapps/roxana/tarea/MainActivity.java
+++ app/src/main/java/com/roxyapps/roxana/tarea/MainActivity.java
@@ -1,13 +1,38 @@
 package com.roxyapps.roxana.tarea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.roxyapps.roxana.tarea.utils.AppConstants;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText EtName, EtPass, EtEmail, EtGender;
+    private Button BtSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EtName = findViewById(R.id.et_Name);
+        EtPass = findViewById(R.id.et_Pass);
+        EtEmail = findViewById(R.id.et_Email);
+        EtGender = findViewById(R.id.et_Gender);
+        BtSend = findViewById(R.id.bt_send);
+
+        //Intent explícito completo
+        BtSend.setOnClickListener(v->{
+            Intent mIntent = new Intent(MainActivity.this, SecondActivity.class);
+            mIntent.putExtra(AppConstants.TEXT_NAME, EtName.getText().toString());
+            mIntent.putExtra(AppConstants.TEXT_PASSWORD, EtPass.getText().toString());
+            mIntent.putExtra(AppConstants.TEXT_EMAIL, EtEmail.getText().toString());
+            mIntent.putExtra(AppConstants.TEXT_GENDER, EtGender.getText().toString());
+            startActivity(mIntent);
+
+        });
     }
 }
--- app/src/main/java/com/roxyapps/roxana/tarea/SecondActivity.java
+++ app/src/main/java/com/roxyapps/roxana/tarea/SecondActivity.java
@@ -1,13 +1,34 @@
 package com.roxyapps.roxana.tarea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.roxyapps.roxana.tarea.utils.AppConstants;
 
 public class SecondActivity extends AppCompatActivity {
+    private TextView TvName, TvPass, TvEmail, TvGender;
+    private Button BtShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TvName = findViewById(R.id.tv_username);
+        TvPass = findViewById(R.id.tv_password);
+        TvEmail = findViewById(R.id.tv_email);
+        TvGender = findViewById(R.id.tv_gender);
+
+
+        Intent mIntent = this.getIntent();
+        if(mIntent != null){
+            TvName.setText(mIntent.getStringExtra(AppConstants.TEXT_NAME));
+            TvPass.setText(mIntent.getStringExtra(AppConstants.TEXT_PASSWORD));
+            TvEmail.setText(mIntent.getStringExtra(AppConstants.TEXT_EMAIL));
+            TvGender.setText(mIntent.getStringExtra(AppConstants.TEXT_GENDER));
+        }
     }
 }
